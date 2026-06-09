34de43106d56c542ae4609e1ae1973bc6a95dcb5
Kailash Kore <korekailash026@gmail.com>
2026-06-09 20:08:39 +0000

    Add monitoring and scripts files

--- scripts/freedns-update.sh
+++ scripts/freedns-update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ELB="a2f04f75604bf4f4986e350eaf4b6bcb-6b8ac33c01639c4c.elb.ap-south-1.amazonaws.com"
+ELB="YOUR_INGRESS_ELB_URL_HERE"
 IP=$(nslookup $ELB | grep Address | tail -1 | awk '{print $2}')
 echo "IP: $IP"
 curl -s "LITELLM_FREEDNS_URL_HERE&address=$IP"
