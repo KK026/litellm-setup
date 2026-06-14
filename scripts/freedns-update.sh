2ba84766be3940bc4d86881e1dbba0b44eb41404
Kailash Kore <korekailash026@gmail.com>
2026-06-14 12:59:44 +0000

    Update ingress configs and add freedns update script

--- scripts/freedns-update.sh
+++ scripts/freedns-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ELB="YOUR_INGRESS_ELB_URL_HERE"
+ELB="a2f04f75604bf4f4986e350eaf4b6bcb-6b8ac33c01639c4c.elb.ap-south-1.amazonaws.com"
 IP=$(nslookup $ELB | grep Address | tail -1 | awk '{print $2}')
 echo "IP: $IP"
-curl -s "LITELLM_FREEDNS_URL_HERE&address=$IP"
-curl -s "GRAFANA_FREEDNS_URL_HERE&address=$IP"
+curl -s "https://freedns.afraid.org/dynamic/update.php?SmcyT0hzdDdENm9DWm5yY3BpUHJjZ1RSOjI2MTU4MTAx&address=$IP"
+curl -s "https://freedns.afraid.org/dynamic/update.php?SmcyT0hzdDdENm9DWm5yY3BpUHJjZ1RSOjI2MTU4MTI0&address=$IP"
 echo "Done!"
